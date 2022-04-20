2ec25f9e1855b4e050df3ccb9a00b48a910888a9
abazanunez <yaa!iQK23%G21z5>
2022-04-20 13:03:58 -0400

    created new publisher entity and repo

--- spring-demo-project/src/main/java/springdemo/springframework/springdemoproject/bootstrap/BootStrapData.java
+++ spring-demo-project/src/main/java/springdemo/springframework/springdemoproject/bootstrap/BootStrapData.java
@@ -4,28 +4,38 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import springdemo.springframework.springdemoproject.domain.Author;
 import springdemo.springframework.springdemoproject.domain.Book;
+import springdemo.springframework.springdemoproject.domain.Publisher;
 import springdemo.springframework.springdemoproject.repositories.AuthorRepository;
 import springdemo.springframework.springdemoproject.repositories.BookRepository;
+import springdemo.springframework.springdemoproject.repositories.PublisherRepository;
 
 @Component
 public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(
+            AuthorRepository authorRepository,
+            BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        //create book and author 1
         Author eric = new Author("alex", "baza");
         Book programming = new Book("Level Programming", "12345");
+
+        //add book and author to books and authors
         eric.getBooks().add(programming);
         programming.getAuthors().add(eric);
 
+        //save book and author to database
         authorRepository.save(eric);
         bookRepository.save(programming);
 
@@ -37,9 +47,13 @@ public class BootStrapData implements CommandLineRunner {
         authorRepository.save(zero);
         bookRepository.save(dogs);
 
+        Publisher publisher1 = new Publisher("penguin","123 Nowhere Street","Morris", "AK", 30481);
+
+        publisherRepository.save(publisher1);
+
         System.out.println("Bootstrap started this");
         System.out.println("Number of books " + bookRepository.count());
-
-
+        System.out.println("publisher " + publisher1);
+        System.out.println("number of publishers " + publisherRepository.count());
     }
 }
